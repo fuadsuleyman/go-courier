56faff99826645fae420602883670abe16806df6
fuadsuleyman <fuadsuleymanov@outlook.com>
2021-11-19 15:41:32 +0400

    change in connection

--- database/connection.go
+++ database/connection.go
@@ -2,8 +2,11 @@ package database
 
 import (
 	"fmt"
+	"log"
+
 	"github.com/fuadsuleyman/go-couriers/models"
-	"github.com/spf13/viper"
+
+	// "github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,27 +15,33 @@ var DB *gorm.DB
 // auth_db_1
 func Connect() {
 
-	db, err = gorm.Open("postgres", "host=192.168.31.74  user=lezzetly password=lezzetly123 dbname=db_name port=5432 sslmode=disable Timezone=Asia/Baku")
+	// db, err := gorm.Open("postgres", "host=192.168.31.74  user=lezzetly password=lezzetly123 dbname=db_name port=5432 sslmode=disable Timezone=Asia/Baku")
 
-	if err != nil {
-		fmt.Println(err, "Error is  here")
-		log.Println("Connection Failed to Open")
-	} else {
-		log.Println("Connection Established")
+	// if err != nil {
+	// 	fmt.Println(err, "Error is  here")
+	// 	log.Println("Connection Failed to Open")
+	// } else {
+	// 	log.Println("Connection Established")
+	// }
+	// DB = db
 
-	DB = db
-	// x := gorm.Open
+	x := gorm.Open
+	connection, err := x(postgres.Open("host=192.168.31.74  user=lezzetly password=lezzetly123 dbname=db_name port=5432 sslmode=disable Timezone=Asia/Baku"))
 
 	
 	// connection, err := x(postgres.Open(fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s TimeZone=%s",
 	// viper.GetString("db.host"), viper.GetString("db.port"), viper.GetString("db.user"), viper.GetString("db.dbname"), viper.GetString("db.password"), viper.GetString("db.sslmode"), viper.GetString("db.TimeZone"))), &gorm.Config{})
 
-	// if err != nil {
-	// 	panic("could not connect to the database!")
-	// }
+	if err != nil {
+		fmt.Println(err, "Error is  here")
+		panic("could not connect to the database!")
+	}else {
+		log.Println("Connection Established")
+	}
 
-	// DB = connection
+	DB = connection
 
-	connection.AutoMigrate(&models.Courier{})
+	DB.AutoMigrate(&models.Courier{})
+
 }
 
