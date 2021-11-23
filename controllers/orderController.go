49efa6cac7a2c2b4aeea99b9ec4efb8815235040
fuadsuleyman <fuadsuleymanov@outlook.com>
2021-11-23 09:18:24 +0400

    PickOrder first part

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	// "log"
 	"github.com/fuadsuleyman/go-couriers/database"
-	// "github.com/fuadsuleyman/go-couriers/helper"
+	"github.com/fuadsuleyman/go-couriers/helper"
 	"github.com/fuadsuleyman/go-couriers/models"
 	"github.com/gofiber/fiber/v2"
 )
@@ -27,6 +27,56 @@ func GetOrders(c *fiber.Ctx) error {
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		ordersMessage: orders,
+	})
+
+}
+
+func PickOrder(c *fiber.Ctx) error {
+	fmt.Println("Hi, from GetCourier!")
+	header := c.Get("Authorization")
+	resMap := helper.CheckToken(header)
+
+	if _, ok := resMap["warning"]; ok {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"warning": resMap["warning"],
+		})
+	}
+	fmt.Println("resMap: ", resMap)
+
+	if resMap["Usertype"] != "2" {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"warning": "Only Courier have not permission to pick up order!",
+		})
+	}
+
+	// check order with param
+	id := c.Params("id")
+
+	var order models.Order
+
+	database.DB.Find(&order, "id = ?", id)
+
+	print("Tapdigim orderin id-si:", order.ID)
+
+	notExistsMesssage := fmt.Sprintf("Order with id %v is not exists!", id)
+
+	if order.ID == 0 {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"warning": notExistsMesssage,
+		})
+	}
+
+	// check courier
+	username := resMap["Username"]
+
+	var currentCourier models.Courier
+
+	database.DB.Find(&currentCourier, "username = ?", username)
+	// currentCourier deyilse error
+	fmt.Println("currentCourier: ", currentCourier)
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"warning": "terminala bax",
 	})
 
 }
